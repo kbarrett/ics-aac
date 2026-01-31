2fbc37964a012bae4181272c9c01896f6c338af0
Kim Barrett <kimbarrett92@gmail.com>
2026-01-31 13:32:25 +0000

    Use display none to hide elements.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 function closePopup(event)
 {
-    document.getElementById("main").style.visibility = "visible";
+    document.getElementById("main").style.display = "block";
     let popup = document.getElementById("popup");
     let popupFlags = popup.getElementsByClassName("flag");
     for (const popupFlag of popupFlags) {
         popup.removeChild(popupFlag);
     }
-    popup.style.visibility = "collapse";
+    popup.style.display = "none";
 }
 
 function popup(event) {
-    if (document.getElementById("main").style.visibility === "collapse") {
+    if (document.getElementById("main").style.display === "none") {
         return;
     }
-    document.getElementById("main").style.visibility = "collapse";
+    document.getElementById("main").style.display = "none";
     let popup = document.getElementById("popup");
     popup.appendChild(event.currentTarget.cloneNode(true));
-    popup.style.visibility = "visible";
+    popup.style.display = "flex";
 }
